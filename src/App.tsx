152ec466c9e11ed7f35eb58ef0fbce72d7388e49
Nick Habelsberger <nick@habelsberger.com>
2024-09-16 14:57:25 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Moin!</p>
+      <span>
+        Ihr seid am richtigen Ort angekommen. Hier ist bald das Inventarsystem
+        zu finden.
+      </span>
+    </>
+  );
+}
+
+export default App;
